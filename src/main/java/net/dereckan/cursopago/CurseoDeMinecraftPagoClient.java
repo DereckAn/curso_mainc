e6a6be987da9531f1025a870bb47f542a9a4cd4b
DereckAn <dereckangeles15071998@gmail.com>
2024-11-26 03:58:16 -0700

    first commit

---
+++ src/main/java/net/dereckan/cursopago/CurseoDeMinecraftPagoClient.java
@@ -0,0 +1,11 @@
+package net.dereckan.cursopago;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CurseoDeMinecraftPagoClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dereckan/cursopago/CursoDeMinecraftPagoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dereckan.cursopago;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CursoDeMinecraftPagoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
