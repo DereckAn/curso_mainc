00ab110bf19f35a72026747598ea3239852ea574
DereckAn <dereckangeles15071998@gmail.com>
2025-02-08 12:23:39 -0700

    Introduction to mixin

---
+++ src/main/java/net/dereckan/cursopago/mixin/AbstractClientPlayerEntityMixin.java
@@ -0,0 +1,45 @@
+package net.dereckan.cursopago.mixin;
+
+import com.mojang.authlib.GameProfile;
+import net.dereckan.cursopago.item.ModItems;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
+    public AbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
+        super(world, pos, yaw, gameProfile);
+    }
+
+    @Inject(method = "getFovMultiplier", at = @At(value = "TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+    private void getFovMultiplierMixin(CallbackInfoReturnable<Float> info, float f) {
+        Item item = this.getActiveItem().getItem();
+        ItemStack itemStack = this.getActiveItem();
+        if (this.isUsingItem()) {
+            if (itemStack.isOf(ModItems.FLUORITE_BOW)) {
+                int i = this.getItemUseTime();
+                float g = (float) i / 20.0F;
+                if (g > 1.0F) {
+                    g = 1.0F;
+                } else {
+                    g *= g;
+                }
+
+                f *= 1.0F - g * 0.15F;
+                info.setReturnValue(MathHelper.lerp(MinecraftClient.getInstance().options.getFovEffectScale().getValue().floatValue(), 1.0F, f));
+            }
+        }
+    }
+}
